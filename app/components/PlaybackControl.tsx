616ccc6db849e6edf7834aa6c887ff0e14d37262
Adri Blanco <adriblabla@gmail.com>
2025-08-12 18:30:33 +0200

    feat: added animations to playback controls and card

--- app/components/PlaybackControl.tsx
+++ app/components/PlaybackControl.tsx
@@ -1,4 +1,5 @@
-import { Image, TouchableOpacity } from "react-native";
+import { useRef, useEffect } from "react";
+import { TouchableOpacity, Animated, Easing } from "react-native";
 
 const PlayIcon = require("../assets/play.png");
 const PauseIcon = require("../assets/pause.png");
@@ -9,14 +10,56 @@ interface PlaybackControlProps {
 }
 
 function PlaybackControl({ isPlaying, togglePlay }: PlaybackControlProps) {
+  const bounceAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (isPlaying) {
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(bounceAnim, {
+            toValue: -5,
+            duration: 600,
+            easing: Easing.linear,
+            useNativeDriver: true,
+          }),
+          Animated.timing(bounceAnim, {
+            toValue: 0,
+            duration: 600,
+            easing: Easing.linear,
+            useNativeDriver: true,
+          }),
+          Animated.timing(bounceAnim, {
+            toValue: 5,
+            duration: 600,
+            easing: Easing.linear,
+            useNativeDriver: true,
+          }),
+          Animated.timing(bounceAnim, {
+            toValue: 0,
+            duration: 600,
+            easing: Easing.linear,
+            useNativeDriver: true,
+          }),
+        ])
+      ).start();
+    } else {
+      bounceAnim.stopAnimation();
+      bounceAnim.setValue(0);
+    }
+  }, [isPlaying, bounceAnim]);
+
   return (
     <TouchableOpacity
       className="flex-row items-center justify-between p-4"
       onPress={togglePlay}
     >
-      <Image
+      <Animated.Image
         source={isPlaying ? PauseIcon : PlayIcon}
-        style={{ width: 300, height: 300 }}
+        style={{
+          width: 300,
+          height: 300,
+          transform: [{ translateY: bounceAnim }],
+        }}
       />
     </TouchableOpacity>
   );
--- app/components/SongCard.tsx
+++ app/components/SongCard.tsx
@@ -1,21 +1,32 @@
-import { Image, View } from "react-native";
+import { Animated, Image, View } from "react-native";
 import Text from "./Text";
 import { Song } from "../db/schema";
+import { useEffect, useRef } from "react";
 
 function SongCard({ song }: { song: Song }) {
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  }, [opacity]);
+
   return (
-    <View className="p-4 rounded-lg items-center">
+    <Animated.View className="p-4 rounded-lg items-center" style={{ opacity }}>
       <Text className="text-9xl">{song.release}</Text>
       <Image
         className="rounded-lg"
         source={{ uri: song.cover }}
         style={{ width: 300, height: 300 }}
       />
-      <View className="mt-8 items-center">
+      <View className="mt-8 items-center gap-4">
         <Text className="text-3xl font-bold">{song.title}</Text>
         <Text className="text-3xl">{song.artist}</Text>
       </View>
-    </View>
+    </Animated.View>
   );
 }
 
